fe3e6e1e4b36eb3bb0c6e18543a562d51a1fc2c2
SergSmilya <sergsmilichenko@gmail.com>
2023-01-08 18:36:00 +0200

    task_1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+let intervalId = null;
+
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stoptBtn: document.querySelector('[data-stop]'),
+};
+
+refs.startBtn.addEventListener('click', onStartInterval);
+refs.stoptBtn.addEventListener('click', onStopInterval);
+
+function onStartInterval() {
+  refs.startBtn.setAttribute('disabled', false);
+  intervalId = setInterval(changeBodyColor, 1000);
+}
+
+function changeBodyColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onStopInterval() {
+  clearInterval(intervalId);
+  refs.startBtn.removeAttribute('disabled', false);
+}
