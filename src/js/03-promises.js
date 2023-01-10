de900d1c4a6c855df91e7b76118e4d86c699db59
SergSmilya <sergsmilichenko@gmail.com>
2023-01-10 18:23:19 +0200

    fixed task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,22 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  formEl: document.querySelector('.form'),
+};
+
+const formValue = {};
+
+refs.formEl.addEventListener('click', createFormValue);
+
+function createFormValue(e) {
+  formValue[e.target.name] = e.target.value;
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   }
 }
