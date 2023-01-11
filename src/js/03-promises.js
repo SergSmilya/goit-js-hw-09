0c9c4276bd3c42d4695c5139a14eabc880daef76
SergSmilya <sergsmilichenko@gmail.com>
2023-01-11 12:19:30 +0200

    fix task3 v2.2

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,22 +1,59 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import throttle from 'lodash.throttle';
 
 const refs = {
   formEl: document.querySelector('.form'),
+  btnFormEl: document.querySelector('.form button'),
 };
 
 const formValue = {};
+let count = 1;
+let step = 0;
 
-refs.formEl.addEventListener('click', createFormValue);
+refs.formEl.addEventListener('input', throttle(onRecordValueFormData, 250));
+refs.btnFormEl.addEventListener('click', onCreatePromiseHandleButton);
 
-function createFormValue(e) {
+function onRecordValueFormData(e) {
   formValue[e.target.name] = e.target.value;
 }
 
+function onCreatePromiseHandleButton(e) {
+  e.preventDefault();
+
+  const intervalId = setInterval(() => {
+    if (count > Number(formValue.amount)) {
+      clearInterval(intervalId);
+      return;
+    }
+    step += Number(formValue.delay) + Number(formValue.step);
+    createPromise(count, step).then(success).catch(error);
+  }, Number(formValue.step));
+
+  refs.formEl.reset();
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  } else {
-    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  }
+  count += 1;
+
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+}
+
+function success(result) {
+  console.log(result);
+  Notify.success(`${result}`);
+}
+
+function error(error) {
+  console.log(error);
+  Notify.failure(`${error}`);
 }
