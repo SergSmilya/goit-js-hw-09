8edb792a96c5863fbcb6dd484479bcb6c79a371c
SergSmilya <sergsmilichenko@gmail.com>
2023-01-12 21:11:04 +0200

    fixed task 2 v2.5

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,57 +1,74 @@
-// import flatpickr from 'flatpickr';
-// import 'flatpickr/dist/flatpickr.min.css';
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-// const options = {
-//   enableTime: true,
-//   time_24hr: true,
-//   defaultDate: new Date(),
-//   minuteIncrement: 1,
-//   // minDate: 'today',
-//   onClose(selectedDates) {
-//     console.log(selectedDates[0]);
-//   },
-// };
+const options = {
+  enableTime: false,
+  // enableTime: true,
 
-// const instance = flatpickr('#datetime-picker', options);
-// const DEFAULTDAY = instance.config.defaultDate.getTime();
-// console.log(DEFAULTDAY);
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  // minDate: 'today',
+  onClose(selectedDates) {
+    // console.log(selectedDates[0]);
+  },
+};
 
-// const refs = {
-//   startBtn: document.querySelector('[data-start]'),
-// };
+const instance = flatpickr('#datetime-picker', options);
+const DEFAULTDAY = instance.config.defaultDate.getTime();
+console.log(DEFAULTDAY);
+let SELECTEDDAYS = 0;
 
-// refs.startBtn.setAttribute('disabled', true);
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+};
 
-// instance.config.onChange.push(function (selectedDates, dateStr, instance) {
-//   const SELECTEDDAYS = selectedDates[0].getTime();
-//   console.log(SELECTEDDAYS);
+refs.startBtn.setAttribute('disabled', true);
+refs.startBtn.addEventListener('click', onStartTimer);
 
-//   if (SELECTEDDAYS > DEFAULTDAY) {
-//     refs.startBtn.removeAttribute('disabled', true);
-//   } else {
-//     refs.startBtn.setAttribute('disabled', true);
-//     Notify.success('Please choose a date in the future');
-//   }
-// });
+instance.config.onChange.push(function (selectedDates, dateStr, instance) {
+  const SELECTEDDAYS = selectedDates[0].getTime();
+  console.log(SELECTEDDAYS);
+
+  if (SELECTEDDAYS > DEFAULTDAY) {
+    refs.startBtn.removeAttribute('disabled', true);
+  } else {
+    refs.startBtn.setAttribute('disabled', true);
+    Notify.success('Please choose a date in the future');
+  }
+
+  const retailTime = SELECTEDDAYS - DEFAULTDAY;
+  console.log(retailTime);
+  const { days, hours, minutes, seconds } = convertMs(retailTime);
+
+  console.log(`${seconds}:${minutes}:${hours}:${days}`);
+});
+
+// console.log(SELECTEDDAYS - DEFAULTDAY);
+
+function onStartTimer() {
+  timer.start();
+}
 
 const timer = {
   start() {
-    const startTime = Date.now();
+    // const startTime = Date.now();
 
-    // setInterval(() => {
-    //   const currentTime = Date.now();
-    //   const retailTime = currentTime - startTime;
-    //   const { days, hours, minutes, seconds } = convertMs(retailTime);
+    const intervslId = setInterval(() => {
+      // const currentTime = Date.now();
+      const retailTime = SELECTEDDAYS - DEFAULTDAY;
+      console.log(retailTime);
+      const { days, hours, minutes, seconds } = convertMs(retailTime);
 
-    //   console.log(`${seconds}`);
-    // }, 1000);
+      console.log(`${seconds}`);
+    }, 1000);
   },
 
-  stop() {},
+  stop() {
+    clearInterval(intervslId);
+  },
 };
-
-timer.start();
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
