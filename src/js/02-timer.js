a07515b84b7937d00d734e474aa37cab23615ef2
SergSmilya <sergsmilichenko@gmail.com>
2023-01-13 18:27:26 +0200

    final

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -9,25 +9,32 @@ const options = {
   minuteIncrement: 1,
   // ! minDate: 'today',
   onClose(selectedDates) {
-    // console.log(selectedDates[0]);
+    selectedDates[0];
   },
 };
 
 const instance = flatpickr('#datetime-picker', options);
+
 const DEFAULTTIME = instance.config.now.getTime();
+let differenceTime = 0;
+let isActive = false;
+let intervalId = null;
+let choseTime = 0;
 
 const refs = {
   startBtn: document.querySelector('[data-start]'),
+  secondsEl: document.querySelector('[data-seconds]'),
+  minutesEl: document.querySelector('[data-minutes]'),
+  hoursEl: document.querySelector('[data-hours]'),
+  daysEl: document.querySelector('[data-days]'),
 };
 
 refs.startBtn.setAttribute('disabled', true);
 
 refs.startBtn.addEventListener('click', onStartTimer);
 
-function onStartTimer() {}
-
 instance.config.onClose.push(function (selectedDates) {
-  let choseTime = selectedDates[0].getTime();
+  choseTime = selectedDates[0].getTime();
 
   if (DEFAULTTIME < choseTime) {
     refs.startBtn.removeAttribute('disabled', true);
@@ -36,16 +43,32 @@ instance.config.onClose.push(function (selectedDates) {
     Notify.success('Please choose a date in the future');
   }
 
-  let differenceTime = choseTime - DEFAULTTIME;
-
-  // const intervalID = setInterval(() => {
-  //   differenceTime -= 1000;
-
-  //   console.log(convertMs(differenceTime));
-  // }, 1000);
+  differenceTime = choseTime - DEFAULTTIME;
 });
 
-console.log(instance.config.onClose);
+function onStartTimer() {
+  isActive = true;
+  if (isActive) {
+    refs.startBtn.setAttribute('disabled', true);
+  }
+
+  intervalId = setInterval(() => {
+    differenceTime -= 1000;
+
+    if (differenceTime <= 1000) {
+      clearInterval(intervalId);
+      isActive = false;
+      refs.startBtn.removeAttribute('disabled', true);
+      document.querySelector('body').style.backgroundColor = 'red';
+      Notify.success('Restart timer');
+    }
+
+    // console.log(convertMs(differenceTime));
+    const totalTimer = convertMs(differenceTime);
+
+    updateClockface(totalTimer);
+  }, 1000);
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -68,4 +91,11 @@ function convertMs(ms) {
 
 function pad(value) {
   return String(value).padStart(2, '0');
+}
+
+function updateClockface({ days, hours, minutes, seconds }) {
+  refs.secondsEl.textContent = `${seconds}`;
+  refs.minutesEl.textContent = `${minutes}`;
+  refs.hoursEl.textContent = `${hours}`;
+  refs.daysEl.textContent = `${days}`;
 }
